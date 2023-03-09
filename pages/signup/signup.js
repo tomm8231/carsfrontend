97f4f90adbb853654f8acccbb178a5da56eb1099
tomm8231 <tomm8231@stud.kea.dk>
2023-03-09 15:11:26 +0100

    member signup done

--- pages/signup/signup.js
+++ pages/signup/signup.js
@@ -9,4 +9,43 @@ export function initSignup() {
 }
 
 
+async function addNewMember() {
+    const username = document.querySelector("#input-username").value
+    const email = document.querySelector("#input-email").value
+    const password = document.querySelector("#input-password").value
+    const firstName = document.querySelector("#input-firstname").value
+    const lastName =  document.querySelector("#input-lastname").value
+    const street =  document.querySelector("#input-street").value
+    const city =  document.querySelector("#input-city").value
+    const zip =  document.querySelector("#input-zip").value
 
+    const newMember = {
+        username: username,
+        email: email,
+        password: password,
+        firstName: firstName,
+        lastName: lastName,
+        steet: street,
+        city: city,
+        zip: zip
+    }
+
+    const options = makeOptions("POST", newMember)
+
+    try {
+        await fetch(URL, options).then(handleHttpErrors)
+    } catch (err) {
+        console.log(err.message)
+    }
+
+    document.querySelector("#input-username").value = ""
+    document.querySelector("#input-email").value = ""
+    document.querySelector("#input-password").value = ""
+    document.querySelector("#input-firstname").value = ""
+    document.querySelector("#input-lastname").value = ""
+    document.querySelector("#input-street").value = ""
+    document.querySelector("#input-city").value = ""
+    document.querySelector("#input-zip").value = ""
+
+    document.querySelector("#status").innerText = "Member added"
+}
