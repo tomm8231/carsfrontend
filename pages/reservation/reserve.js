9fa531c07407ae3a082f145d280e5e703ca453b7
tomm8231 <tomm8231@stud.kea.dk>
2023-03-12 16:32:07 +0100

    last push before handing in

--- pages/reservation/reserve.js
+++ pages/reservation/reserve.js
@@ -1,11 +1,70 @@
+import { API_URL } from "../../settings.js";
+import { 
+    makeOptions,
+    handleHttpErrors,
+    sanitizeStringWithTableRows 
+} from "../../utils.js";
 
-import { API_URL } from "../../settings.js"
-const URL = API_URL + "/cars"
+const carsURL = API_URL + "/cars";
 
 export async function initReservation() {
+  fetchAllCars();
+  document.querySelector("#table-rows").onclick = setupReservationModal
+  document.querySelector("#btn-reservation").onclick = makeNewReservation
+}
 
+async function fetchAllCars() {
+  try {
+    const cars = await fetch(carsURL).then(handleHttpErrors)
+
+    const tableRows = cars
+      .map(
+        (car) => `
+    <tr>
+      <td>${car.id}</td>
+      <td>${car.brand}</td>
+      <td>${car.model}</td>
+      <td>${car.pricePrDay}</td>
+    <td>
+    <button id="reserve-row-btn_${car.id}" type="button" class="btn btn-sm btn-secondary" data-bs-toggle="modal" data-bs-target="#reservation-modal">Reserve car</button></td>
+    </tr>`
+      )
+      .join("\n")
+
+    document.querySelector("#table-rows").innerHTML = sanitizeStringWithTableRows(tableRows)
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 async function setupReservationModal(evt) {
+  const target = evt.target
+  const parts = target.id.split("_")
+  const id = parts[1]
+  document.querySelector("#car-id").value = id
 }
 
+async function makeNewReservation() {
+  const carId = document.querySelector("#car-id").value;
+  const username = document.querySelector("#user-name").value;
+  const reservationDate = document.querySelector("#reservation-date").value;
+
+  const newReservation = { 
+    carId: carId,
+    username: username, 
+    rentalDate: reservationDate 
+}
+
+  const options = makeOptions("POST", newReservation);
+
+
+  const reservationURL = API_URL + "/reservations";
+
+  try {
+    const reservation = await fetch(reservationURL, options).then(handleHttpErrors);
+    document.querySelector("#user-name").innerText = reservation.username;
+    document.querySelector("#status").innerText = `Reservation for ${username} made`;
+  } catch (error) {
+    document.querySelector("#status").innerText = error.message;
+  }
+}
--- pages/showReservations/reservations.js
+++ pages/showReservations/reservations.js
@@ -1,5 +1,37 @@
-import { API_URL} from "../../settings.js"
+import { API_URL } from "../../settings.js"
+import { 
+    handleHttpErrors,
+    sanitizeStringWithTableRows 
+} from "../../utils.js";
+
+const URL = API_URL + "/reservations"
 
 export async function initListReservationsAll() {
+  fetchAllReservations();
 }
 
+export async function fetchAllReservations() {
+  try {
+    const reservations = await fetch(API_URL + "/reservations").then(handleHttpErrors);
+    const cars = await fetch(API_URL + "/cars").then(handleHttpErrors);
+
+    const carMap = new Map(cars.map((car) => [car.id, car]));
+
+    const tableRows = reservations
+      .map((reservation) => {
+        const car = carMap.get(reservation.carId);
+        return `
+            <tr>
+              <td>${reservation.carId}</td>
+              <td>${car.brand}</td>
+              <td>${car.model}</td>
+              <td>${reservation.rentalDate}</td>
+              <td>${car.pricePrDay}</td>
+            </tr>`;
+      }).join("\n");
+
+    document.querySelector("#tablerows").innerHTML = sanitizeStringWithTableRows(tableRows);
+  } catch (err) {
+    console.log(err);
+  }
+}
